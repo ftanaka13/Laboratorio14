55d4ef0457e6ed37e95914173528b105fb51053d
Fernando Tanaka <fernando.tanaka@outlook.com>
2024-09-16 20:20:42 -0300

    viewBinding

--- app/src/main/java/br/com/faculdadeimpacta/laboratorio14/ui/MainActivity.kt
+++ app/src/main/java/br/com/faculdadeimpacta/laboratorio14/ui/MainActivity.kt
@@ -5,17 +5,32 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.faculdadeimpacta.laboratorio14.R
+import br.com.faculdadeimpacta.laboratorio14.data.Tarefa
+import br.com.faculdadeimpacta.laboratorio14.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+    private val listaDeTarefas: MutableList<Tarefa> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
